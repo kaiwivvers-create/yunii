d54ffb9af55aee548b411e623f77da36ad4376d0
Kaikai <evellolyqqe@gmail.com>
2026-08-05 15:36:41 +0700

    a

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService, User } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login(@Body() loginDto: { email: string; password: string }) {
+  login(@Body() loginDto: { email: string; password: string }): Promise<{ access_token: string; user: User }> {
     return this.authService.login(loginDto);
   }
 
   @Post('signup')
-  signup(@Body() signupDto: { name: string; email: string; password: string }) {
+  signup(@Body() signupDto: { name: string; email: string; password: string }): Promise<{ access_token: string; user: User }> {
     return this.authService.signup(signupDto);
   }
 }
